58dacff65bd7624a6c5a86226484f92a424c8939
erfan <erfan.call.11@gmail.com>
2025-11-27 18:41:25 +0330

    add package

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    screens:{
+      "desktop":"1200px",
+      "mobile":"320px",
+    },
+  },
+  plugins: [],
+}
+
